9d98a66f15ea8790462fc055cec4b06acee1e9f0
Marike Elwers <m.e.elwers@gmail.com>
2020-11-11 14:33:18 +0100

    zwischenstand

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Lupita', address: 'Bergmannstraße 135 Berlin', phone_number: '04012356', category: 'italian')
+Restaurant.create(name: 'Black Coffee', address: 'Lobeckstraße 140 Hamburg', phone_number: '04012890', category: 'french')
+Restaurant.create(name: 'Frau Moeller', address: 'Lange Reihe 30 Hamburg', phone_number: '04333890', category: 'belgian')
+Restaurant.create(name: 'Cox', address: 'Lange Reihe 60 Hamburg', phone_number: '04444890', category: 'french')
+Restaurant.create(name: 'Opote', address: 'Lange Reihe 70 Hamburg', phone_number: '04123890', category: 'italian')
