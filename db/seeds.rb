b307a562dd1a915c490450f0804d4b99a787f5f3
Marike Elwers <m.e.elwers@gmail.com>
2020-11-11 16:00:48 +0100

    zwischenlösung

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+puts "create restaurants"
 Restaurant.create(name: 'Lupita', address: 'Bergmannstraße 135 Berlin', phone_number: '04012356', category: 'italian')
 Restaurant.create(name: 'Black Coffee', address: 'Lobeckstraße 140 Hamburg', phone_number: '04012890', category: 'french')
 Restaurant.create(name: 'Frau Moeller', address: 'Lange Reihe 30 Hamburg', phone_number: '04333890', category: 'belgian')
 Restaurant.create(name: 'Cox', address: 'Lange Reihe 60 Hamburg', phone_number: '04444890', category: 'french')
 Restaurant.create(name: 'Opote', address: 'Lange Reihe 70 Hamburg', phone_number: '04123890', category: 'italian')
+puts "created restaurants"
